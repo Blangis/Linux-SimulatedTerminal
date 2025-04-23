5e46182ec4f4afe8d177423b541bc1dacf4b2360
Giselle L <blancag.licona@gmail.com>
2025-04-23 00:00:37 -0600

    [Gis][Add][main] Add help command that shows information about available commands

---
+++ ayuda.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cat << HELP
+╔═══════════════════════════════════════╗
+║ ¡Aquí puedes encontrar los comandos!  ║
+║═══════════════════════════════════════║
+║ 👋saludo: Muestra un saludo.          ║
+║ 🆘ayuda: Menú de comandos disponibles.║
+║ 🔍buscar: Busca un archivo en una     ║
+║          carpeta.                     ║
+║ 🛈 infosis: Muestra información del    ║
+║           sistema.                    ║
+║ ⏰fecha: Muestra fecha y hora.        ║
+║ 💻creditos: Muestra créditos de los   ║
+║            programadores.             ║
+║ 🎮juego: Despliega un juego.          ║
+║ 🎵musica: Abre reproductor mp3.       ║
+║ 🚪salir: Sale de la terminal.         ║
+║                                       ║
+║                                       ║
+║                                       ║
+║                                       ║
+╚═══════════════════════════════════════╝
+
+HELP
+
+
