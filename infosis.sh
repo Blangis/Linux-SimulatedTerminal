625cfd9f2bd25c56ff954921b5d2eddef2d98342
Giselle L <blancag.licona@gmail.com>
2025-04-22 22:37:51 -0600

    [Gis][Change][main]Change layout of infosis.sh

--- infosis.sh
+++ infosis.sh
@@ -9,17 +9,14 @@ magenta=$(tput setaf 5)
 cyan=$(tput setaf 6)
 
 reset=$(tput sgr0)
-
-clear 
-tput cup 2 0
+ 
 echo "${cyan}Memoria RAM:${magenta} $(grep MemTotal /proc/meminfo | cut -d ":" -f2 | xargs)${reset}"
 
-tput cup 3 0
 if grep -q lm /proc/cpuinfo ; then 
 	echo "${cyan}Arquitectura del sistema:${magenta}64 bits${reset}"
 else 
 	echo "${cyan}Arquitectura del sistema:${magenta}32 bits${reset}"
 fi
 
-tput cup 4 0
+
 echo "${cyan}Versión del SO:${magenta}$(grep -i "^version=" /etc/os-release | cut -d "=" -f2)${reset}"
